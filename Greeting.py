25a552ce6adf29d0a4e06f0b64e61e90c81c71ff
Utkarsh Atroulia <130576865+utkarshatroulia@users.noreply.github.com>
2025-04-28 17:05:26 +0530

    Add files via upload

---
+++ Greeting.py
@@ -0,0 +1,5 @@
+name1 = input('Enter a first name: ')
+name2 = input('Enter a second name:')
+
+fullname = name1+ '' +name2
+print('Hello,' ,fullname + '! Welcome to the python program')
---
+++ mathematicaloperation.py
@@ -0,0 +1,7 @@
+a= float(input('Enter the first number:'))
+b = float(input('Enter the second number:'))
+
+print('addition:', a+b)
+print('Subraction:', a-b)
+print('Multiplication:', a*b)
+print('Division:', a/b)
